8f483058bb62ad50d0ac490d71f4dfc4f357ddd5
airdropbaaz <125475579+airdropbaaz@users.noreply.github.com>
2024-04-17 14:51:24 +0330

    Update ERC1155PreAuthUpgradeable.sol

--- contracts/nft/ERC1155PreAuthUpgradeable.sol
+++ contracts/nft/ERC1155PreAuthUpgradeable.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
